4f2010360e613ef5a19c03fef01205e6dbf16d57
Charles J Hardy <chuckjhardy@gmail.com>
2015-10-09 20:24:57 +0100

    Add Run Container Object

---
+++ lib/cloud_scrape/run.rb
@@ -0,0 +1,17 @@
+require "cloud_scrape/run_dto"
+
+class CloudScrape
+  class Run
+    def initialize(id:)
+      @id = id
+    end
+
+    def execute
+      RunDTO.for(id: id, url: "executions")
+    end
+
+    private
+
+    attr_reader :id
+  end
+end
---
+++ spec/cloud_scrape/run_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe CloudScrape::Run do
+  let(:instance) { described_class.new(id: id) }
+
+  let(:id) { "tE2e9y7J-eyFiOAKaivrxsMl" }
+
+  describe "#execute" do
+    subject(:execute) { instance.execute }
+
+    let(:run) { {} }
+
+    it "calls off to RunDTO and returns", :aggregate_failures do
+      expect(CloudScrape::RunDTO).to receive(:for)
+        .with(id: id, url: "executions") { run }
+
+      expect(execute).to eq(run)
+    end
+  end
+end
